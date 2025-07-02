0f8433f11d79938757370bf818fdc044fda222f1
Isamu Mogi <isamu@leafytree.jp>
2025-07-02 21:35:18 +0900

    chore: no set-location on build

--- build/src/ubuntu-noble/build.ps1
+++ build/src/ubuntu-noble/build.ps1
@@ -4,27 +4,25 @@ $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 Set-StrictMode -Version 3.0
 
-Set-Location $PSScriptRoot
-
-$outputContent = Get-Content "header.bat" -Raw -Encoding UTF8
+$outputContent = Get-Content (Join-Path $PSScriptRoot "header.bat") -Raw -Encoding UTF8
 $outputContent += "`n"
 
 $outputContent += "######################################## PowerShell ########################################`n"
-$outputContent += Get-Content "main.ps1" -Raw -Encoding UTF8
+$outputContent += Get-Content (Join-Path $PSScriptRoot "main.ps1") -Raw -Encoding UTF8
 $outputContent += "`n"
 
 $outputContent += "<# ##################################### Dockerfile ########################################`n"
-$outputContent += Get-Content "Dockerfile" -Raw -Encoding UTF8
+$outputContent += Get-Content (Join-Path $PSScriptRoot "Dockerfile") -Raw -Encoding UTF8
 $outputContent += "######################################## Dockerfile ##################################### #>`n"
 $outputContent += "`n"
 
 $outputContent += "<# ##################################### entrypoint.sh ########################################`n"
-$outputContent += Get-Content "entrypoint.sh" -Raw -Encoding UTF8
+$outputContent += Get-Content (Join-Path $PSScriptRoot "entrypoint.sh") -Raw -Encoding UTF8
 $outputContent += "######################################## entrypoint.sh ##################################### #>`n"
 $outputContent += "`n"
 
 $outputContent += "<# ##################################### ai-sandbox.rdp ########################################`n"
-$outputContent += Get-Content "ai-sandbox.rdp" -Raw -Encoding Unicode
+$outputContent += Get-Content (Join-Path $PSScriptRoot "ai-sandbox.rdp") -Raw -Encoding Unicode
 $outputContent += "password 51:b:### HASH ###`n"
 $outputContent += "######################################## ai-sandbox.rdp ##################################### #>`n"
 
